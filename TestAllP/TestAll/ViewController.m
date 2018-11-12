1f6e017bdf6727e06457c6dbb922dc9721b4b39f
zgy5352 <zhaoguoying2009@163.com>
2018-11-12 10:39:25 +0800

    Add files via upload

---
+++ TestAllP/TestAll/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  TestAll
+//
+//  Created by zhaoguoying on 2017/9/27.
+//  Copyright © 2017年 ZDHS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self _initView1];
+}
+#pragma mark - _init
+#pragma mark - delegate
+#pragma mark - action
+#pragma mark - other
+-(void)_initView1{
+    
+   
+    
+}
+
+
+@end
